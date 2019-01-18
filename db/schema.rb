99bbc75f42ce326af2b9cb23fbb55cd6a8691182
Chaonan Chen <chaonan.chen01@stclairconnect.ca>
2019-01-18 08:54:25 -0500

    Add movies table

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190118042816) do
+ActiveRecord::Schema.define(version: 20190118134825) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "author"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "movies", force: :cascade do |t|
+    t.string "name"
+    t.string "director"
+    t.integer "year_released"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
